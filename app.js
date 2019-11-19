4630f8c7156bfd786726776c5d32234493349be1
beata <beata@emc2wave.com>
2019-11-18 17:53:25 -0800

    correct error

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhone);
 
 function validateName() {
+  const name = document.getElementById('name');
+  const re = /^[a-zA-Z]{2,10}$/;
 
+  if(!re.test(name.value)) {
+    name.classList.add('is-invalid');
+  } else {
+    name.classList.remove('is-invalid');
+  }
 }
 
 function validateZip() {
